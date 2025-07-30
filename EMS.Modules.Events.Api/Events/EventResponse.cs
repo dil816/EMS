622d571038e1cd4bd49db00e355b5905b349ec68
dil816 <Rivindudilochana18329@gmail.com>
2025-07-30 20:19:40 +0530

    event endpoints added

---
+++ EMS.Modules.Events.Api/Events/EventResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace EMS.Modules.Events.Api.Events;
+
+public sealed record EventResponse(
+    Guid Id,
+    string Title,
+    string Description,
+    string Location,
+    DateTime StartsAtUtc,
+    DateTime? EndsAtUtc);
+
---
+++ EMS.Modules.Events.Api/Tags.cs
@@ -0,0 +1,7 @@
+﻿namespace EMS.Modules.Events.Api.Events;
+
+internal static class Tags
+{
+    internal const string Events = "Events";
+}
+
