0eaf597e5f8085d6eadaa9d8c81c1a441de9e77c
dil816 <Rivindudilochana18329@gmail.com>
2025-07-30 20:16:07 +0530

    EventdbContext added

---
+++ EMS.Modules.Events.Api/Database/EventsDbContext.cs
@@ -0,0 +1,12 @@
+﻿using EMS.Modules.Events.Api.Events;
+using Microsoft.EntityFrameworkCore;
+
+namespace EMS.Modules.Events.Api.Database;
+public sealed class EventsDbContext(DbContextOptions<EventsDbContext> options) : DbContext(options)
+{
+    internal DbSet<Event> Events { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema(Schema.Events);
+    }
+}
