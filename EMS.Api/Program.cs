98b26507a74fc4c9bce0a98d953631b75c4919c6
dil816 <Rivindudilochana18329@gmail.com>
2025-07-30 21:05:28 +0530

    migration extension added

--- EMS.Api/Program.cs
+++ EMS.Api/Program.cs
@@ -1,7 +1,11 @@
+using EMS.Api.Extensions;
+using EMS.Modules.Events.Api;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddEventsModule(builder.Configuration);
 
 WebApplication app = builder.Build();
 
@@ -9,6 +13,10 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    app.ApplyMigrations();
 }
+
+EventsModule.MapEndPoints(app);
 
 app.Run();
