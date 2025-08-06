6fa6e169532e7ab49d4e42b81d2b06b10c03a987
dil816 <Rivindudilochana18329@gmail.com>
2025-08-06 18:24:56 +0530

    initial ticket model added

---
+++ EMS.Modules.Events.Domain/TicketTypes/TicketType.cs
@@ -0,0 +1,42 @@
+﻿using EMS.Modules.Events.Domain.Abstractions;
+using EMS.Modules.Events.Domain.Events;
+
+namespace EMS.Modules.Events.Domain.TicketTypes;
+public sealed class TicketType : Entity
+{
+    private TicketType()
+    {
+    }
+
+    public Guid Id { get; private set; }
+
+    public Guid EventId { get; private set; }
+
+    public string Name { get; private set; }
+
+    public decimal Price { get; private set; }
+
+    public string Currency { get; private set; }
+
+    public decimal Quantity { get; private set; }
+
+    public static TicketType Create(
+        Event @event,
+        string name,
+        decimal price,
+        string currency,
+        decimal quantity)
+    {
+        var ticketType = new TicketType
+        {
+            Id = Guid.NewGuid(),
+            EventId = @event.Id,
+            Name = name,
+            Price = price,
+            Currency = currency,
+            Quantity = quantity
+        };
+
+        return ticketType;
+    }
+}
