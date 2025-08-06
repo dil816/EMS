8d14b9da88e46123d85931bec48f3df4663d83e8
dil816 <Rivindudilochana18329@gmail.com>
2025-08-06 23:46:13 +0530

    Event repository new method added

--- EMS.Modules.Events.Domain/Events/IEventRepository.cs
+++ EMS.Modules.Events.Domain/Events/IEventRepository.cs
@@ -1,5 +1,7 @@
 ﻿namespace EMS.Modules.Events.Domain.Events;
 public interface IEventRepository
 {
+    Task<Event?> GetAsync(Guid id, CancellationToken cancellationToken = default);
+
     void Insert(Event @event);
 }
