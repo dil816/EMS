ec10b8759dc8ff7a9940704afe7b3056c20a6184
dil816 <Rivindudilochana18329@gmail.com>
2025-08-06 12:55:05 +0530

    domain Event opertions  added

---
+++ EMS.Modules.Events.Domain/Events/EventCanceledDomainEvent.cs
@@ -0,0 +1,7 @@
+﻿using EMS.Modules.Events.Domain.Abstractions;
+
+namespace EMS.Modules.Events.Domain.Events;
+internal class EventCanceledDomainEvent(Guid eventId) : DomainEvent
+{
+    public Guid EventId { get; init; } = eventId;
+}
---
+++ EMS.Modules.Events.Domain/Events/EventPublishedDomainEvent.cs
@@ -0,0 +1,7 @@
+﻿using EMS.Modules.Events.Domain.Abstractions;
+
+namespace EMS.Modules.Events.Domain.Events;
+public sealed class EventPublishedDomainEvent(Guid eventId) : DomainEvent
+{
+    public Guid EventId { get; init; } = eventId;
+}
