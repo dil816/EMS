aa1657ef10ef4b6d0b8f3e7f9d4331e57dd2c54e
dil816 <Rivindudilochana18329@gmail.com>
2025-08-07 00:39:08 +0530

    publish event command and handlers added

---
+++ EMS.Modules.Events.Application/Events/PublishEvent/PublishEventCommand.cs
@@ -0,0 +1,5 @@
+﻿using EMS.Modules.Events.Application.Abstractions.Messaging;
+
+namespace EMS.Modules.Events.Application.Events.PublishEvent;
+public sealed record PublishEventCommand(Guid EventId) : ICommand;
+
