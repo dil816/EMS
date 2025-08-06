a4281fde447fd3e0f9a6d56e8c27c92dcb0e66eb
dil816 <Rivindudilochana18329@gmail.com>
2025-08-06 23:06:21 +0530

    ticket type response record class added

---
+++ EMS.Modules.Events.Application/Events/GetEvent/TicketTypeResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace EMS.Modules.Events.Application.Events.GetEvent;
+public sealed record class TicketTypeResponse(
+    Guid TicketTypeId,
+    string Name,
+    decimal Price,
+    string Currency,
+    decimal Quantity);
