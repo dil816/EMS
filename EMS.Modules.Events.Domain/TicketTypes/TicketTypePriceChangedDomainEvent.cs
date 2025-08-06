31285de9286f4c10d774f65a0aa31085fbab528c
dil816 <Rivindudilochana18329@gmail.com>
2025-08-06 18:49:36 +0530

    ticket repository interface added

--- EMS.Modules.Events.Domain/TicketTypes/TicketTypePriceChangedDomainEvent.cs
+++ EMS.Modules.Events.Domain/TicketTypes/TicketTypePriceChangedDomainEvent.cs
@@ -1,7 +1,7 @@
 ﻿using EMS.Modules.Events.Domain.Abstractions;
 
 namespace EMS.Modules.Events.Domain.TicketTypes;
-internal class TicketTypePriceChangedDomainEvent(Guid ticketTypeId, decimal price) : DomainEvent
+public sealed class TicketTypePriceChangedDomainEvent(Guid ticketTypeId, decimal price) : DomainEvent
 {
     public Guid TicketTypeId { get; init; } = ticketTypeId;
     public decimal Price { get; init; } = price;
