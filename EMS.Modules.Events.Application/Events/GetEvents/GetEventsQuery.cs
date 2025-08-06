489bde5b8258b8ca73f91cac9b86086a301c6021
dil816 <Rivindudilochana18329@gmail.com>
2025-08-06 23:31:29 +0530

    get events query added

---
+++ EMS.Modules.Events.Application/Events/GetEvents/GetEventsQuery.cs
@@ -0,0 +1,4 @@
+﻿using EMS.Modules.Events.Application.Abstractions.Messaging;
+
+namespace EMS.Modules.Events.Application.Events.GetEvents;
+public sealed record GetEventsQuery : IQuery<IReadOnlyCollection<EventResponse>>;
