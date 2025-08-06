e5c60f08a339b32272c89e70c3a26c429cee6078
dil816 <Rivindudilochana18329@gmail.com>
2025-08-06 18:42:50 +0530

    ticket error type class added

---
+++ EMS.Modules.Events.Domain/TicketTypes/TicketTypeErrors.cs
@@ -0,0 +1,11 @@
+﻿using EMS.Modules.Events.Domain.Abstractions;
+
+namespace EMS.Modules.Events.Domain.TicketTypes;
+public static class TicketTypeErrors
+{
+    public static Error NotFound(Guid id) =>
+        Error.NotFound(
+            "TicketTypes.NotFound",
+            $"Ticket type with ID '{id}' was not found."
+        );
+}
