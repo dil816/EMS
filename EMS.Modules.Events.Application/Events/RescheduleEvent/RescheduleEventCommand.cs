990a068138ff27573d7d801f9bf2a70782f5f486
dil816 <Rivindudilochana18329@gmail.com>
2025-08-07 00:52:53 +0530

    Reschedule event command handle added

---
+++ EMS.Modules.Events.Application/Events/RescheduleEvent/RescheduleEventCommand.cs
@@ -0,0 +1,4 @@
+﻿using EMS.Modules.Events.Application.Abstractions.Messaging;
+
+namespace EMS.Modules.Events.Application.Events.RescheduleEvent;
+public sealed record RescheduleEventCommand(Guid EventId, DateTime StartsAtUtc, DateTime? EndsAtUtc) : ICommand;
---
+++ EMS.Modules.Events.Application/Events/RescheduleEvent/RescheduleEventCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using EMS.Modules.Events.Application.Abstractions.Clock;
+using EMS.Modules.Events.Application.Abstractions.Data;
+using EMS.Modules.Events.Application.Abstractions.Messaging;
+using EMS.Modules.Events.Domain.Abstractions;
+using EMS.Modules.Events.Domain.Events;
+
+namespace EMS.Modules.Events.Application.Events.RescheduleEvent;
+internal sealed class RescheduleEventCommandHandler(
+    IDateTimeProvider dateTimeProvider,
+    IEventRepository eventRepository,
+    IUnitOfWork unitOfWork)
+    : ICommandHandler<RescheduleEventCommand>
+{
+    public async Task<Result> Handle(RescheduleEventCommand request, CancellationToken cancellationToken)
+    {
+        Event? @event = await eventRepository.GetAsync(request.EventId, cancellationToken);
+
+        if (@event is null)
+        {
+            return Result.Failure(EventErrors.NotFound(request.EventId));
+        }
+
+        if (request.StartsAtUtc < dateTimeProvider.UtcNow)
+        {
+            return Result.Failure(EventErrors.StartDateInPast);
+        }
+
+        @event.Reschedule(request.StartsAtUtc, request.EndsAtUtc);
+
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Result.Success();
+    }
+}
