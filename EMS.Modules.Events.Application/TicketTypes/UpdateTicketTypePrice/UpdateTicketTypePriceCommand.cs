5377dc9244f1eb8df890917768a602a2ade3ff05
dil816 <Rivindudilochana18329@gmail.com>
2025-08-07 18:36:52 +0530

    update ticket type price command and handler added

---
+++ EMS.Modules.Events.Application/TicketTypes/UpdateTicketTypePrice/UpdateTicketTypePriceCommand.cs
@@ -0,0 +1,5 @@
+﻿using EMS.Modules.Events.Application.Abstractions.Messaging;
+
+namespace EMS.Modules.Events.Application.TicketTypes.UpdateTicketTypePrice;
+public sealed record UpdateTicketTypePriceCommand(Guid TicketTypeId, decimal Price) : ICommand;
+
