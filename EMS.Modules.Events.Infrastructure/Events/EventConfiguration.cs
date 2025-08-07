2475eee16831082d534fc99814f9ca4636b66230
dil816 <Rivindudilochana18329@gmail.com>
2025-08-07 22:16:07 +0530

    Event and Category relation configuration added

---
+++ EMS.Modules.Events.Infrastructure/Events/EventConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using EMS.Modules.Events.Domain.Categories;
+using EMS.Modules.Events.Domain.Events;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EMS.Modules.Events.Infrastructure.Events;
+internal sealed class EventConfiguration : IEntityTypeConfiguration<Event>
+{
+    public void Configure(EntityTypeBuilder<Event> builder)
+    {
+        builder.HasOne<Category>().WithMany();
+    }
+}
