8e9bafc0ea0870912c513de8e7ba46b8e75d81a5
dil816 <Rivindudilochana18329@gmail.com>
2025-08-07 18:10:39 +0530

    get ticket type query and handlers added

---
+++ EMS.Modules.Events.Application/TicketTypes/GetTicketType/GetTicketTypeQuery.cs
@@ -0,0 +1,4 @@
+﻿using EMS.Modules.Events.Application.Abstractions.Messaging;
+
+namespace EMS.Modules.Events.Application.TicketTypes.GetTicketType;
+public sealed record GetTicketTypeQuery(Guid TicketTypeId) : IQuery<TicketTypeResponse>;
