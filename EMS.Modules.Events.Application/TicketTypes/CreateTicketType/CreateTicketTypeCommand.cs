6b5f5af2b9adec62b50205cb37ca6e2f068b00f4
dil816 <Rivindudilochana18329@gmail.com>
2025-08-07 17:57:11 +0530

    create tickrt type handlers and commands added

---
+++ EMS.Modules.Events.Application/TicketTypes/CreateTicketType/CreateTicketTypeCommand.cs
@@ -0,0 +1,9 @@
+﻿using EMS.Modules.Events.Application.Abstractions.Messaging;
+
+namespace EMS.Modules.Events.Application.TicketTypes.CreateTicketType;
+public sealed record CreateTicketTypeCommand(
+    Guid EventId,
+    string Name,
+    decimal Price,
+    string Currency,
+    decimal Quantity) : ICommand<Guid>;
---
+++ EMS.Modules.Events.Application/TicketTypes/CreateTicketType/CreateTicketTypeCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using EMS.Modules.Events.Application.Abstractions.Data;
+using EMS.Modules.Events.Application.Abstractions.Messaging;
+using EMS.Modules.Events.Domain.Abstractions;
+using EMS.Modules.Events.Domain.Events;
+using EMS.Modules.Events.Domain.TicketTypes;
+
+namespace EMS.Modules.Events.Application.TicketTypes.CreateTicketType;
+internal class CreateTicketTypeCommandHandler(
+    IEventRepository eventRepository,
+    ITicketTypeRepository ticketTypeRepository,
+    IUnitOfWork unitOfWork)
+    : ICommandHandler<CreateTicketTypeCommand, Guid>
+{
+    public async Task<Result<Guid>> Handle(CreateTicketTypeCommand request, CancellationToken cancellationToken)
+    {
+        Event? @event = await eventRepository.GetAsync(request.EventId, cancellationToken);
+
+        if (@event is null)
+        {
+            return Result.Failure<Guid>(EventErrors.NotFound(request.EventId));
+        }
+
+        var ticketType = TicketType.Create(@event, request.Name, request.Price, request.Currency, request.Quantity);
+
+        ticketTypeRepository.Insert(ticketType);
+
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return ticketType.Id;
+    }
+}
