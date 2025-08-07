3cb5912b163775d22d3315be957ac65aeab55024
dil816 <Rivindudilochana18329@gmail.com>
2025-08-07 22:09:31 +0530

    Date time provider class added

---
+++ EMS.Modules.Events.Infrastructure/Clock/DateTimeProvider.cs
@@ -0,0 +1,7 @@
+﻿using EMS.Modules.Events.Application.Abstractions.Clock;
+
+namespace EMS.Modules.Events.Infrastructure.Clock;
+internal sealed class DateTimeProvider : IDateTimeProvider
+{
+    public DateTime UtcNow => DateTime.UtcNow;
+}
