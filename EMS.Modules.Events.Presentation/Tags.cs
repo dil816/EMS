81748c2977cce2942fa2d230868906b14cc14fd6
dil816 <Rivindudilochana18329@gmail.com>
2025-08-07 22:59:11 +0530

    new tags  added for Tickettype and category

--- EMS.Modules.Events.Presentation/Tags.cs
+++ EMS.Modules.Events.Presentation/Tags.cs
@@ -3,5 +3,9 @@
 internal static class Tags
 {
     internal const string Events = "Events";
+
+    internal const string TicketTypes = "TicketTypes";
+
+    internal const string Categories = "Categories";
 }
 
