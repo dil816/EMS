f7980e33167e0b85a0c55f736cae2929f7bd6dad
dil816 <Rivindudilochana18329@gmail.com>
2025-08-07 18:28:38 +0530

    get ticket types by event Query and handler added

---
+++ EMS.Modules.Events.Application/TicketTypes/GetTicketTypes/GetTicketTypesQuery.cs
@@ -0,0 +1,6 @@
+﻿using EMS.Modules.Events.Application.Abstractions.Messaging;
+using EMS.Modules.Events.Application.TicketTypes.GetTicketType;
+
+namespace EMS.Modules.Events.Application.TicketTypes.GetTicketTypes;
+public sealed record GetTicketTypesQuery(Guid EventId) : IQuery<IReadOnlyCollection<TicketTypeResponse>>;
+
---
+++ EMS.Modules.Events.Application/TicketTypes/GetTicketTypes/GetTicketTypesQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Data.Common;
+using Dapper;
+using EMS.Modules.Events.Application.Abstractions.Data;
+using EMS.Modules.Events.Application.Abstractions.Messaging;
+using EMS.Modules.Events.Application.TicketTypes.GetTicketType;
+using EMS.Modules.Events.Domain.Abstractions;
+
+namespace EMS.Modules.Events.Application.TicketTypes.GetTicketTypes;
+internal sealed class GetTicketTypesQueryHandler(IDbConnectionFactory dbConnectionFactory)
+    : IQueryHandler<GetTicketTypesQuery, IReadOnlyCollection<TicketTypeResponse>>
+{
+    public async Task<Result<IReadOnlyCollection<TicketTypeResponse>>> Handle(
+        GetTicketTypesQuery request,
+        CancellationToken cancellationToken)
+    {
+        await using DbConnection connection = await dbConnectionFactory.OpenConnectionAsync();
+
+        const string sql =
+            $"""
+             SELECT
+                 id AS {nameof(TicketTypeResponse.Id)},
+                 event_id AS {nameof(TicketTypeResponse.EventId)},
+                 name AS {nameof(TicketTypeResponse.Name)},
+                 price AS {nameof(TicketTypeResponse.Price)},
+                 currency AS {nameof(TicketTypeResponse.Currency)},
+                 quantity AS {nameof(TicketTypeResponse.Quantity)}
+             FROM events.ticket_types
+             WHERE event_id = @EventId
+             """;
+        List<TicketTypeResponse> ticketTypes =
+            (await connection.QueryAsync<TicketTypeResponse>(sql, request)).AsList();
+
+        return ticketTypes;
+    }
+}
