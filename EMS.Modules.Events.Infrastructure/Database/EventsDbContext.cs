22b1f31c8ca3f4d7c2df5e6f635fcd68e3376277
dil816 <Rivindudilochana18329@gmail.com>
2025-08-07 22:13:59 +0530

    Category TiketType  Dbsets added and  configurations added

--- EMS.Modules.Events.Infrastructure/Database/EventsDbContext.cs
+++ EMS.Modules.Events.Infrastructure/Database/EventsDbContext.cs
@@ -1,13 +1,25 @@
 ﻿using EMS.Modules.Events.Application.Abstractions.Data;
+using EMS.Modules.Events.Domain.Categories;
 using EMS.Modules.Events.Domain.Events;
+using EMS.Modules.Events.Domain.TicketTypes;
+using EMS.Modules.Events.Infrastructure.Events;
+using EMS.Modules.Events.Infrastructure.TicketTypes;
 using Microsoft.EntityFrameworkCore;
 
 namespace EMS.Modules.Events.Infrastructure.Database;
 public sealed class EventsDbContext(DbContextOptions<EventsDbContext> options) : DbContext(options), IUnitOfWork
 {
     internal DbSet<Event> Events { get; set; }
+
+    internal DbSet<Category> Categories { get; set; }
+
+    internal DbSet<TicketType> TicketTypes { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.HasDefaultSchema(Schema.Events);
+
+        modelBuilder.ApplyConfiguration(new EventConfiguration());
+        modelBuilder.ApplyConfiguration(new TicketTypeConfiguration());
     }
 }
