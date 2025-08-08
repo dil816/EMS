dec95dcc4ac18817e968bd0a537c927e2cf1f53d
dil816 <Rivindudilochana18329@gmail.com>
2025-08-08 22:50:53 +0530

    Exception class added

---
+++ EMS.Modules.Events.Application/Abstractions/Exceptions/EMSException.cs
@@ -0,0 +1,17 @@
+﻿using EMS.Modules.Events.Domain.Abstractions;
+
+namespace EMS.Modules.Events.Application.Abstractions.Exceptions;
+public sealed class EmsException : Exception
+{
+    public EmsException(string requestName, Error? error = default, Exception? innerException = default)
+        : base("Application exception", innerException)
+    {
+        RequestName = requestName;
+        Error = error;
+    }
+
+    public string RequestName { get; }
+
+    public Error? Error { get; }
+}
+
