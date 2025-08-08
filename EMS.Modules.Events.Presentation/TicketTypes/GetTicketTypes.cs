1beab69df0e41b5eeeececd5e222b9e557ceabb4
dil816 <Rivindudilochana18329@gmail.com>
2025-08-08 22:41:42 +0530

    Category anf tickettype modules added

---
+++ EMS.Modules.Events.Presentation/TicketTypes/GetTicketTypes.cs
@@ -0,0 +1,25 @@
+﻿using EMS.Modules.Events.Application.TicketTypes.GetTicketType;
+using EMS.Modules.Events.Application.TicketTypes.GetTicketTypes;
+using EMS.Modules.Events.Domain.Abstractions;
+using EMS.Modules.Events.Presentation.ApiResults;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace EMS.Modules.Events.Presentation.TicketTypes;
+
+internal static class GetTicketTypes
+{
+    public static void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("ticket-types", async (Guid eventId, ISender sender) =>
+        {
+            Result<IReadOnlyCollection<TicketTypeResponse>> result = await sender.Send(
+                new GetTicketTypesQuery(eventId));
+
+            return result.Match(Results.Ok, ApiResults.ApiResults.Problem);
+        })
+        .WithTags(Tags.TicketTypes);
+    }
+}
