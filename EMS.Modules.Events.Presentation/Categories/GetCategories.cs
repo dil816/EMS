6dee1157571d20a3bb507de4ffaa8cbff224e7df
dil816 <Rivindudilochana18329@gmail.com>
2025-08-15 12:09:40 +0530

    cacheing added to categories endpoint

--- EMS.Modules.Events.Presentation/Categories/GetCategories.cs
+++ EMS.Modules.Events.Presentation/Categories/GetCategories.cs
@@ -1,4 +1,5 @@
-﻿using EMS.Common.Domain;
+﻿using EMS.Common.Application.Caching;
+using EMS.Common.Domain;
 using EMS.Modules.Events.Application.Categories.GetCategories;
 using EMS.Modules.Events.Application.Categories.GetCategory;
 using EMS.Modules.Events.Presentation.ApiResults;
@@ -13,9 +14,22 @@ internal static class GetCategories
 {
     public static void MapEndpoint(IEndpointRouteBuilder app)
     {
-        app.MapGet("categories", async (ISender sender) =>
+        app.MapGet("categories", async (ISender sender, ICacheService cacheService) =>
         {
+            IReadOnlyCollection<CategoryResponse> categoryResponses =
+                await cacheService.GetAsync<IReadOnlyCollection<CategoryResponse>>("categories");
+
+            if (categoryResponses is not null)
+            {
+                return Results.Ok(categoryResponses);
+            }
+
             Result<IReadOnlyCollection<CategoryResponse>> result = await sender.Send(new GetCategoriesQuery());
+
+            if (result.IsSuccess)
+            {
+                await cacheService.SetAsync("categories", result.Value);
+            }
 
             return result.Match(Results.Ok, ApiResults.ApiResults.Problem);
         })
