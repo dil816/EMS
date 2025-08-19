6313cf763985b9ad703c3641b0da135894a7e8c7
dil816 <Rivindudilochana18329@gmail.com>
2025-08-19 19:05:12 +0530

    ticketing module added

--- EMS.Modules.Ticketing.Infrastructure/TicketingModule.cs
+++ EMS.Modules.Ticketing.Infrastructure/TicketingModule.cs
@@ -1,5 +1,24 @@
-﻿namespace EMS.Modules.Ticketing.Infrastructure;
+﻿using EMS.Common.Presentation.EndPoints;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EMS.Modules.Ticketing.Infrastructure;
 
 public static class TicketingModule
 {
+    public static IServiceCollection AddTicketingModule(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddInfrastructure(configuration);
+
+        services.AddEndpoints(Presentation.AssemblyReference.Assembly);
+
+        return services;
+    }
+
+    private static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        // TODO
+    }
 }
