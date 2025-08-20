9d08856c73904bc85cdc97985f909a74aca72dbf
dil816 <Rivindudilochana18329@gmail.com>
2025-08-20 23:15:16 +0530

    get cart usecase added

---
+++ EMS.Modules.Ticketing.Application/Carts/GetCart/GetCartQuery.cs
@@ -0,0 +1,5 @@
+﻿using EMS.Common.Application.Messaging;
+
+namespace EMS.Modules.Ticketing.Application.Carts.GetCart;
+public sealed record GetCartQuery(Guid CustomerId) : IQuery<Cart>;
+
---
+++ EMS.Modules.Ticketing.Application/Carts/GetCart/GetCartQueryHandler.cs
@@ -0,0 +1,11 @@
+﻿using EMS.Common.Application.Messaging;
+using EMS.Common.Domain;
+
+namespace EMS.Modules.Ticketing.Application.Carts.GetCart;
+internal sealed class GetCartQueryHandler(CartService cartService) : IQueryHandler<GetCartQuery, Cart>
+{
+    public async Task<Result<Cart>> Handle(GetCartQuery request, CancellationToken cancellationToken)
+    {
+        return await cartService.GetAsync(request.CustomerId, cancellationToken);
+    }
+}
