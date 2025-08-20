cf9bc76dc1f3efa51cb18a96f7a83246ff6530d2
dil816 <Rivindudilochana18329@gmail.com>
2025-08-21 00:07:54 +0530

    custoer by id e=query added for ticketing

---
+++ EMS.Modules.Ticketing.Application/Customers/GetCustomer/GetCustomerQuery.cs
@@ -0,0 +1,4 @@
+﻿using EMS.Common.Application.Messaging;
+
+namespace EMS.Modules.Ticketing.Application.Customers.GetCustomer;
+public sealed record GetCustomerQuery(Guid CustomerId) : IQuery<CustomerResponse>;
