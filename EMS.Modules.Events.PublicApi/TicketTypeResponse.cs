568b902386b36ff5e56f799865800534eefe3b6c
dil816 <Rivindudilochana18329@gmail.com>
2025-08-20 16:04:13 +0530

    public api project added

---
+++ EMS.Modules.Events.PublicApi/TicketTypeResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace EMS.Modules.Events.PublicApi;
+
+public sealed record TicketTypeResponse(
+    Guid Id,
+    Guid EventId,
+    string Name,
+    decimal Price,
+    string Currency,
+    decimal Quantity);
