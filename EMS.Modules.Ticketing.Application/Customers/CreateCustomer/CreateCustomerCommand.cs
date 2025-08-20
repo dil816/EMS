8e1e0e2b571d848b647ee38487f5766147af6d4b
dil816 <Rivindudilochana18329@gmail.com>
2025-08-20 23:54:25 +0530

    create customer use case add for the ticketing module

---
+++ EMS.Modules.Ticketing.Application/Customers/CreateCustomer/CreateCustomerCommand.cs
@@ -0,0 +1,5 @@
+﻿using EMS.Common.Application.Messaging;
+
+namespace EMS.Modules.Ticketing.Application.Customers.CreateCustomer;
+public sealed record CreateCustomerCommand(Guid CustomerId, string Email, string FirstName, string LastName)
+    : ICommand;
---
+++ EMS.Modules.Ticketing.Application/Customers/CreateCustomer/CreateCustomerCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using EMS.Common.Application.Messaging;
+using EMS.Common.Domain;
+using EMS.Modules.Ticketing.Application.Abstractions;
+using EMS.Modules.Ticketing.Domain.Customers;
+
+namespace EMS.Modules.Ticketing.Application.Customers.CreateCustomer;
+
+internal sealed class CreateCustomerCommandHandler(ICustomerRepository customerRepository, IUnitOfWork unitOfWork)
+    : ICommandHandler<CreateCustomerCommand>
+{
+    public async Task<Result> Handle(CreateCustomerCommand request, CancellationToken cancellationToken)
+    {
+        var customer = Customer.Create(request.CustomerId, request.Email, request.FirstName, request.LastName);
+
+        customerRepository.Insert(customer);
+
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Result.Success();
+    }
+}
