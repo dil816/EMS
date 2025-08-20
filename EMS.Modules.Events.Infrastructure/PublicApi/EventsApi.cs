98e5eedb320950f06da4703decf40d5ce72e2ed2
dil816 <Rivindudilochana18329@gmail.com>
2025-08-20 16:04:31 +0530

    ieventapi impl added

---
+++ EMS.Modules.Events.Infrastructure/PublicApi/EventsApi.cs
@@ -0,0 +1,30 @@
+﻿using EMS.Common.Domain;
+using EMS.Modules.Events.Application.TicketTypes.GetTicketType;
+using EMS.Modules.Events.PublicApi;
+using MediatR;
+using TicketTypeResponse = EMS.Modules.Events.PublicApi.TicketTypeResponse;
+
+namespace EMS.Modules.Events.Infrastructure.PublicApi;
+internal sealed class EventsApi(ISender sender) : IEventsApi
+{
+    public async Task<TicketTypeResponse?> GetTicketTypeAsync(
+        Guid ticketTypeId,
+        CancellationToken cancellationToken = default)
+    {
+        Result<Application.TicketTypes.GetTicketType.TicketTypeResponse> result =
+            await sender.Send(new GetTicketTypeQuery(ticketTypeId), cancellationToken);
+
+        if (result.IsFailure)
+        {
+            return null;
+        }
+
+        return new TicketTypeResponse(
+            result.Value.Id,
+            result.Value.EventId,
+            result.Value.Name,
+            result.Value.Price,
+            result.Value.Currency,
+            result.Value.Quantity);
+    }
+}
