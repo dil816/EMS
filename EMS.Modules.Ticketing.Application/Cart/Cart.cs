e09f326195b2fb74e4c193baf077870d819e7e23
dil816 <Rivindudilochana18329@gmail.com>
2025-08-20 12:14:17 +0530

    cart service added

---
+++ EMS.Modules.Ticketing.Application/Cart/Cart.cs
@@ -0,0 +1,9 @@
+﻿namespace EMS.Modules.Ticketing.Application.Cart;
+public sealed class Cart
+{
+    public Guid CustomerId { get; init; }
+
+    public List<CartItem> Items { get; init; } = [];
+
+    internal static Cart CreateDefault(Guid customerId) => new() { CustomerId = customerId };
+}
