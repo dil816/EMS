d56a636e3abb73e633e7f420f7d9b1455982d601
dil816 <Rivindudilochana18329@gmail.com>
2025-08-21 11:19:49 +0530

    customer repository added

---
+++ EMS.Modules.Ticketing.Infrastructure/Customers/CustomerRepository.cs
@@ -0,0 +1,17 @@
+﻿using EMS.Modules.Ticketing.Domain.Customers;
+using EMS.Modules.Ticketing.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace EMS.Modules.Ticketing.Infrastructure.Customers;
+internal sealed class CustomerRepository(TicketingDbContext context) : ICustomerRepository
+{
+    public async Task<Customer?> GetAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        return await context.Customers.SingleOrDefaultAsync(c => c.Id == id, cancellationToken);
+    }
+
+    public void Insert(Customer customer)
+    {
+        context.Customers.Add(customer);
+    }
+}
