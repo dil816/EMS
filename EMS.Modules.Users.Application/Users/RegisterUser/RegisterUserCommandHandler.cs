1b761c9b8153bf3aac127dfcc1d7b2030e802f46
dil816 <Rivindudilochana18329@gmail.com>
2025-08-21 13:33:22 +0530

    ticketing public api calling method added

--- EMS.Modules.Users.Application/Users/RegisterUser/RegisterUserCommandHandler.cs
+++ EMS.Modules.Users.Application/Users/RegisterUser/RegisterUserCommandHandler.cs
@@ -1,10 +1,14 @@
 ﻿using EMS.Common.Application.Messaging;
 using EMS.Common.Domain;
+using EMS.Modules.Ticketing.PublicApi;
 using EMS.Modules.Users.Application.Abstractions.Data;
 using EMS.Modules.Users.Domain.Users;
 
 namespace EMS.Modules.Users.Application.Users.RegisterUser;
-internal sealed class RegisterUserCommandHandler(IUserRepository userRepository, IUnitOfWork unitOfWork)
+internal sealed class RegisterUserCommandHandler(
+    IUserRepository userRepository,
+    IUnitOfWork unitOfWork,
+    ITicketingApi ticketingApi)
     : ICommandHandler<RegisterUserCommand, Guid>
 {
     public async Task<Result<Guid>> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
@@ -14,6 +18,8 @@ internal sealed class RegisterUserCommandHandler(IUserRepository userRepository,
         userRepository.Insert(user);
 
         await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        await ticketingApi.CreateCustomerAsync(user.Id, user.Email, user.FirstName, user.LastName, cancellationToken);
 
         return user.Id;
     }
