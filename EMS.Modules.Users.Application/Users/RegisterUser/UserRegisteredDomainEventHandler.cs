5ca4dfc3f30812fef3156a1480bd737ccf38b867
dil816 <Rivindudilochana18329@gmail.com>
2025-08-21 23:24:12 +0530

    user registered domain event handler added

---
+++ EMS.Modules.Users.Application/Users/RegisterUser/UserRegisteredDomainEventHandler.cs
@@ -0,0 +1,29 @@
+﻿using EMS.Common.Application.Exceptions;
+using EMS.Common.Application.Messaging;
+using EMS.Common.Domain;
+using EMS.Modules.Ticketing.PublicApi;
+using EMS.Modules.Users.Application.Users.GetUser;
+using EMS.Modules.Users.Domain.Users;
+using MediatR;
+
+namespace EMS.Modules.Users.Application.Users.RegisterUser;
+internal sealed class UserRegisteredDomainEventHandler(ISender sender, ITicketingApi ticketingApi)
+    : IDomainEventHandler<UserRegisteredDomainEvent>
+{
+    public async Task Handle(UserRegisteredDomainEvent notification, CancellationToken cancellationToken)
+    {
+        Result<UserResponse> result = await sender.Send(new GetUserQuery(notification.UserId), cancellationToken);
+
+        if (result.IsFailure)
+        {
+            throw new EmsException(nameof(GetUserQuery), result.Error);
+        }
+
+        await ticketingApi.CreateCustomerAsync(
+            result.Value.Id,
+            result.Value.Email,
+            result.Value.FirstName,
+            result.Value.LastName,
+            cancellationToken);
+    }
+}
