37b1e79e37dfd463dabc2f3dc6bd4d53f6d8d90a
dil816 <Rivindudilochana18329@gmail.com>
2025-08-23 23:51:10 +0530

    Remove item from cart handlers added

---
+++ EMS.Modules.Ticketing.Application/Carts/RemoveItemFromCart/RemoveItemFromCartCommand.cs
@@ -0,0 +1,5 @@
+﻿using EMS.Common.Application.Messaging;
+
+namespace EMS.Modules.Ticketing.Application.Carts.RemoveItemFromCart;
+public sealed record RemoveItemFromCartCommand(Guid CustomerId, Guid TicketTypeId) : ICommand;
+
---
+++ EMS.Modules.Ticketing.Application/Carts/RemoveItemFromCart/RemoveItemFromCartCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace EMS.Modules.Ticketing.Application.Carts.RemoveItemFromCart;
+
+internal sealed class RemoveItemFromCartCommandValidator : AbstractValidator<RemoveItemFromCartCommand>
+{
+    public RemoveItemFromCartCommandValidator()
+    {
+        RuleFor(c => c.CustomerId).NotEmpty();
+        RuleFor(c => c.TicketTypeId).NotEmpty();
+    }
+}
+
