2b2cdcafe7a47344e40d5801ef06e8a18ac6e57a
dil816 <Rivindudilochana18329@gmail.com>
2025-08-24 00:50:59 +0530

    order added query hanlers

---
+++ EMS.Modules.Ticketing.Application/Orders/GetOrder/GetOrderQuery.cs
@@ -0,0 +1,4 @@
+﻿using EMS.Common.Application.Messaging;
+
+namespace EMS.Modules.Ticketing.Application.Orders.GetOrder;
+public sealed record GetOrderQuery(Guid OrderId) : IQuery<OrderResponse>;
---
+++ EMS.Modules.Ticketing.Application/Orders/GetOrder/OrderResponse.cs
@@ -0,0 +1,13 @@
+﻿using EMS.Modules.Ticketing.Domain.Orders;
+
+namespace EMS.Modules.Ticketing.Application.Orders.GetOrder;
+
+public sealed record OrderResponse(
+    Guid Id,
+    Guid CustomerId,
+    OrderStatus Status,
+    decimal TotalPrice,
+    DateTime CreatedAtUtc)
+{
+    public List<OrderItemResponse> OrderItems { get; } = [];
+}
