ec5a5b9969aa8efc046920898d493b0ed0f47b78
dil816 <Rivindudilochana18329@gmail.com>
2025-08-24 01:00:34 +0530

    tickettype price update handler added

---
+++ EMS.Modules.Ticketing.Application/TicketTypes/UpdateTicketTypePrice/UpdateTicketTypePriceCommand.cs
@@ -0,0 +1,4 @@
+﻿using EMS.Common.Application.Messaging;
+
+namespace EMS.Modules.Ticketing.Application.TicketTypes.UpdateTicketTypePrice;
+public sealed record UpdateTicketTypePriceCommand(Guid TicketTypeId, decimal Price) : ICommand;
---
+++ EMS.Modules.Ticketing.Application/TicketTypes/UpdateTicketTypePrice/UpdateTicketTypePriceCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using EMS.Common.Application.Messaging;
+using EMS.Common.Domain;
+using EMS.Modules.Ticketing.Application.Abstractions.Data;
+using EMS.Modules.Ticketing.Domain.Events;
+
+namespace EMS.Modules.Ticketing.Application.TicketTypes.UpdateTicketTypePrice;
+
+internal sealed class UpdateTicketTypePriceCommandHandler(
+    ITicketTypeRepository ticketTypeRepository,
+    IUnitOfWork unitOfWork)
+    : ICommandHandler<UpdateTicketTypePriceCommand>
+{
+    public async Task<Result> Handle(UpdateTicketTypePriceCommand request, CancellationToken cancellationToken)
+    {
+        TicketType? ticketType = await ticketTypeRepository.GetAsync(request.TicketTypeId, cancellationToken);
+
+        if (ticketType is null)
+        {
+            return Result.Failure(TicketTypeErrors.NotFound(request.TicketTypeId));
+        }
+
+        ticketType.UpdatePrice(request.Price);
+
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Result.Success();
+    }
+}
