1b0bfbe74e1b1fdd432a4ffb277c1add17bac97c
dil816 <Rivindudilochana18329@gmail.com>
2025-08-24 00:54:20 +0530

    get order added

---
+++ EMS.Modules.Ticketing.Application/Orders/GetOrders/GetOrdersQuery.cs
@@ -0,0 +1,5 @@
+﻿using EMS.Common.Application.Messaging;
+
+namespace EMS.Modules.Ticketing.Application.Orders.GetOrders;
+public sealed record GetOrdersQuery(Guid CustomerId) : IQuery<IReadOnlyCollection<OrderResponse>>;
+
---
+++ EMS.Modules.Ticketing.Application/Orders/GetOrders/OrderResponse.cs
@@ -0,0 +1,11 @@
+﻿using EMS.Modules.Ticketing.Domain.Orders;
+
+namespace EMS.Modules.Ticketing.Application.Orders.GetOrders;
+
+public sealed record OrderResponse(
+    Guid Id,
+    Guid CustomerId,
+    OrderStatus Status,
+    decimal TotalPrice,
+    DateTime CreatedAtUtc);
+
