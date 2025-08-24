0f0ef5eb5da2cf5cc41344c93b01f847b90901b3
dil816 <Rivindudilochana18329@gmail.com>
2025-08-24 09:11:04 +0530

    new refacs

--- EMS.Modules.Ticketing.Application/Payments/RefundPayment/RefundPaymentCommand.cs
+++ EMS.Modules.Ticketing.Application/Payments/RefundPayment/RefundPaymentCommand.cs
@@ -1,41 +1,4 @@
 ﻿using EMS.Common.Application.Messaging;
-using EMS.Common.Domain;
-using EMS.Modules.Ticketing.Application.Abstractions.Data;
-using EMS.Modules.Ticketing.Domain.Payments;
-using FluentValidation;
 
 namespace EMS.Modules.Ticketing.Application.Payments.RefundPayment;
 public sealed record RefundPaymentCommand(Guid PaymentId, decimal Amount) : ICommand;
-
-internal sealed class RefundPaymentCommandHandler(IPaymentRepository paymentRepository, IUnitOfWork unitOfWork)
-    : ICommandHandler<RefundPaymentCommand>
-{
-    public async Task<Result> Handle(RefundPaymentCommand request, CancellationToken cancellationToken)
-    {
-        Payment? payment = await paymentRepository.GetAsync(request.PaymentId, cancellationToken);
-
-        if (payment is null)
-        {
-            return Result.Failure(PaymentErrors.NotFound(request.PaymentId));
-        }
-
-        Result result = payment.Refund(request.Amount);
-
-        if (result.IsFailure)
-        {
-            return Result.Failure(result.Error);
-        }
-
-        await unitOfWork.SaveChangesAsync(cancellationToken);
-
-        return Result.Success();
-    }
-}
-
-internal sealed class RefundPaymentCommandValidator : AbstractValidator<RefundPaymentCommand>
-{
-    public RefundPaymentCommandValidator()
-    {
-        RuleFor(c => c.PaymentId).NotEmpty();
-    }
-}
---
+++ EMS.Modules.Ticketing.Application/Payments/RefundPayment/RefundPaymentCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using EMS.Common.Application.Messaging;
+using EMS.Common.Domain;
+using EMS.Modules.Ticketing.Application.Abstractions.Data;
+using EMS.Modules.Ticketing.Domain.Payments;
+
+namespace EMS.Modules.Ticketing.Application.Payments.RefundPayment;
+
+internal sealed class RefundPaymentCommandHandler(IPaymentRepository paymentRepository, IUnitOfWork unitOfWork)
+    : ICommandHandler<RefundPaymentCommand>
+{
+    public async Task<Result> Handle(RefundPaymentCommand request, CancellationToken cancellationToken)
+    {
+        Payment? payment = await paymentRepository.GetAsync(request.PaymentId, cancellationToken);
+
+        if (payment is null)
+        {
+            return Result.Failure(PaymentErrors.NotFound(request.PaymentId));
+        }
+
+        Result result = payment.Refund(request.Amount);
+
+        if (result.IsFailure)
+        {
+            return Result.Failure(result.Error);
+        }
+
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Result.Success();
+    }
+}
