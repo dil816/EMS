5a34c65baffc7a97d50d77e1b47e6b2a5c858fed
dil816 <Rivindudilochana18329@gmail.com>
2025-09-04 21:09:22 +0530

    user module architecture tests added

---
+++ EMS.Modules.Users.ArchitectureTests/Abstractions/BaseTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using EMS.Modules.Users.Domain.Users;
+using EMS.Modules.Users.Infrastructure;
+
+namespace EMS.Modules.Users.ArchitectureTests.Abstractions;
+internal static class BaseTest
+{
+    public static readonly Assembly ApplicationAssembly = typeof(Users.Application.AssemblyReference).Assembly;
+
+    public static readonly Assembly DomainAssembly = typeof(User).Assembly;
+
+    public static readonly Assembly InfrastructureAssembly = typeof(UsersModule).Assembly;
+
+    public static readonly Assembly PresentationAssembly = typeof(Users.Presentation.AssemblyReference).Assembly;
+}
---
+++ EMS.Modules.Users.ArchitectureTests/Presentation/PresentationTests.cs
@@ -0,0 +1,31 @@
+﻿using EMS.Modules.Users.ArchitectureTests.Abstractions;
+using MassTransit;
+using NetArchTest.Rules;
+
+namespace EMS.Modules.Users.ArchitectureTests.Presentation;
+public class PresentationTests
+{
+    [Fact]
+    public void IntegrationEventHandler_Should_BeSealed()
+    {
+        Types.InAssembly(BaseTest.PresentationAssembly)
+            .That()
+            .ImplementInterface(typeof(IConsumer<>))
+            .Should()
+            .BeSealed()
+            .GetResult()
+            .ShouldBeSuccessful();
+    }
+
+    [Fact]
+    public void IntegrationEventHandler_ShouldHave_NameEndingWith_DomainEventHandler()
+    {
+        Types.InAssembly(BaseTest.PresentationAssembly)
+            .That()
+            .ImplementInterface(typeof(IConsumer<>))
+            .Should()
+            .HaveNameEndingWith("Consumer")
+            .GetResult()
+            .ShouldBeSuccessful();
+    }
+}
