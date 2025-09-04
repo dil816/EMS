b5e7a184520f80f07d75f907dd8dd40717749712
dil816 <Rivindudilochana18329@gmail.com>
2025-09-04 16:46:11 +0530

    Architecture test cases added for 4 modules

--- EMS.ArchitectureTests/Layers/ModuleTests.cs
+++ EMS.ArchitectureTests/Layers/ModuleTests.cs
@@ -1,12 +1,30 @@
 ﻿using System.Reflection;
 using EMS.ArchitectureTests.Abstractions;
+using EMS.Modules.Attendance.Domain.Attendees;
+using EMS.Modules.Attendance.Infrastructure;
+using EMS.Modules.Events.Domain.Events;
+using EMS.Modules.Events.Infrastructure;
+using EMS.Modules.Ticketing.Domain.Orders;
+using EMS.Modules.Ticketing.Infrastructure;
 using EMS.Modules.Users.Domain.Users;
 using EMS.Modules.Users.Infrastructure;
 using NetArchTest.Rules;
 
 namespace EMS.ArchitectureTests.Layers;
-public class ModuleTests : BaseTest
+public class ModuleTests
 {
+    protected const string UsersNamespace = "EMS.Modules.Users";
+    protected const string UsersIntegrationEventsNamespace = "EMS.Modules.Users.IntegrationEvents";
+
+    protected const string EventsNamespace = "EMS.Modules.Events";
+    protected const string EventsIntegrationEventsNamespace = "EMS.Modules.Events.IntegrationEvents";
+
+    protected const string TicketingNamespace = "EMS.Modules.Ticketing";
+    protected const string TicketingIntegrationEventsNamespace = "EMS.Modules.Ticketing.IntegrationEvents";
+
+    protected const string AttendanceNamespace = "EMS.Modules.Attendance";
+    protected const string AttendanceIntegrationEventsNamespace = "EMS.Modules.Attendance.IntegrationEvents";
+
     [Fact]
     public void UsersModule_ShouldNotHaveDependencyOn_AnyOtherModule()
     {
@@ -34,4 +52,90 @@ public class ModuleTests : BaseTest
             .GetResult()
             .ShouldBeSuccessful();
     }
+
+    [Fact]
+    public void EventsModule_ShouldNotHaveDependencyOn_AnyOtherModule()
+    {
+        string[] otherModules = [UsersNamespace, TicketingNamespace, AttendanceNamespace];
+        string[] integrationEventsModules =
+        [
+            UsersIntegrationEventsNamespace,
+            TicketingIntegrationEventsNamespace,
+            AttendanceIntegrationEventsNamespace
+        ];
+
+        List<Assembly> eventsAssemblies =
+        [
+            typeof(Event).Assembly,
+            Modules.Events.Application.AssemblyReference.Assembly,
+            Modules.Events.Presentation.AssemblyReference.Assembly,
+            typeof(EventsModule).Assembly
+        ];
+
+        Types.InAssemblies(eventsAssemblies)
+            .That()
+            .DoNotHaveDependencyOnAny(integrationEventsModules)
+            .Should()
+            .NotHaveDependencyOnAny(otherModules)
+            .GetResult()
+            .ShouldBeSuccessful();
+    }
+
+    [Fact]
+    public void TicketingModule_ShouldNotHaveDependencyOn_AnyOtherModule()
+    {
+        string[] otherModules = [EventsNamespace, UsersNamespace, AttendanceNamespace];
+        string[] integrationEventsModules =
+        [
+            EventsIntegrationEventsNamespace,
+            UsersIntegrationEventsNamespace,
+            AttendanceIntegrationEventsNamespace
+        ];
+
+        List<Assembly> ticketingAssemblies =
+        [
+            typeof(Order).Assembly,
+            Modules.Ticketing.Application.AssemblyReference.Assembly,
+            Modules.Ticketing.Presentation.AssemblyReference.Assembly,
+            typeof(TicketingModule).Assembly
+        ];
+
+        Types.InAssemblies(ticketingAssemblies)
+            .That()
+            .DoNotHaveDependencyOnAny(integrationEventsModules)
+            .Should()
+            .NotHaveDependencyOnAny(otherModules)
+            .GetResult()
+            .ShouldBeSuccessful();
+    }
+
+    [Fact]
+    public void AttendanceModule_ShouldNotHaveDependencyOn_AnyOtherModule()
+    {
+        string[] otherModules = [UsersNamespace, TicketingNamespace, EventsNamespace];
+        string[] integrationEventsModules =
+        [
+            UsersIntegrationEventsNamespace,
+            TicketingIntegrationEventsNamespace,
+            EventsIntegrationEventsNamespace
+        ];
+
+        List<Assembly> attendanceAssemblies =
+        [
+            typeof(Attendee).Assembly,
+            Modules.Attendance.Application.AssemblyReference.Assembly,
+            Modules.Attendance.Presentation.AssemblyReference.Assembly,
+            typeof(AttendanceModule).Assembly
+        ];
+
+        Types.InAssemblies(attendanceAssemblies)
+            .That()
+            .DoNotHaveDependencyOnAny(integrationEventsModules)
+            .Should()
+            .NotHaveDependencyOnAny(otherModules)
+            .GetResult()
+            .ShouldBeSuccessful();
+    }
 }
+
+
