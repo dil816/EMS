0f63133353c10e1f09aff790ef4a5e52b0182870
dil816 <Rivindudilochana18329@gmail.com>
2025-09-04 16:06:23 +0530

    architecture test project added

---
+++ EMS.ArchitectureTests/Abstractions/BaseTest.cs
@@ -0,0 +1,16 @@
+﻿namespace EMS.ArchitectureTests.Abstractions;
+
+public abstract class BaseTest
+{
+    protected const string UsersNamespace = "EMS.Modules.Users";
+    protected const string UsersIntegrationEventsNamespace = "EMS.Modules.Users.IntegrationEvents";
+
+    protected const string EventsNamespace = "EMS.Modules.Events";
+    protected const string EventsIntegrationEventsNamespace = "EMS.Modules.Events.IntegrationEvents";
+
+    protected const string TicketingNamespace = "EMS.Modules.Ticketing";
+    protected const string TicketingIntegrationEventsNamespace = "EMS.Modules.Ticketing.IntegrationEvents";
+
+    protected const string AttendanceNamespace = "EMS.Modules.Attendance";
+    protected const string AttendanceIntegrationEventsNamespace = "EMS.Modules.Attendance.IntegrationEvents";
+}
---
+++ EMS.ArchitectureTests/Layers/ModuleTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using EMS.ArchitectureTests.Abstractions;
+using EMS.Modules.Users.Domain.Users;
+using EMS.Modules.Users.Infrastructure;
+using NetArchTest.Rules;
+
+namespace EMS.ArchitectureTests.Layers;
+public class ModuleTests : BaseTest
+{
+    [Fact]
+    public void UsersModule_ShouldNotHaveDependencyOn_AnyOtherModule()
+    {
+        string[] otherModules = [EventsNamespace, TicketingNamespace, AttendanceNamespace];
+        string[] integrationEventsModules =
+        [
+            EventsIntegrationEventsNamespace,
+            TicketingIntegrationEventsNamespace,
+            AttendanceIntegrationEventsNamespace
+        ];
+
+        List<Assembly> usersAssemblies =
+        [
+            typeof(User).Assembly,
+            Modules.Users.Application.AssemblyReference.Assembly,
+            Modules.Users.Presentation.AssemblyReference.Assembly,
+            typeof(UsersModule).Assembly
+        ];
+
+        Types.InAssemblies(usersAssemblies)
+            .That()
+            .DoNotHaveDependencyOnAny(integrationEventsModules)
+            .Should()
+            .NotHaveDependencyOnAny(otherModules)
+            .GetResult()
+            .ShouldBeSuccessful();
+    }
+}
