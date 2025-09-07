3f379317a26dc59d619a3d3a93e8ff356da6f9ce
dil816 <Rivindudilochana18329@gmail.com>
2025-09-07 21:15:00 +0530

    Attendence arch tests added

---
+++ EMS.Modules.Attendance.ArchitectureTests/Abstractions/BaseTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using EMS.Modules.Attendance.Domain.Attendees;
+using EMS.Modules.Attendance.Infrastructure;
+
+namespace EMS.Modules.Attendance.ArchitectureTests.Abstractions;
+
+internal static class BaseTest
+{
+    public static readonly Assembly ApplicationAssembly = typeof(Attendance.Application.AssemblyReference).Assembly;
+
+    public static readonly Assembly DomainAssembly = typeof(Attendee).Assembly;
+
+    public static readonly Assembly InfrastructureAssembly = typeof(AttendanceModule).Assembly;
+
+    public static readonly Assembly PresentationAssembly = typeof(Attendance.Presentation.AssemblyReference).Assembly;
+}
---
+++ EMS.Modules.Attendance.ArchitectureTests/Presentation/PresentationTests.cs
@@ -0,0 +1,31 @@
+﻿using EMS.Modules.Attendance.ArchitectureTests.Abstractions;
+using MassTransit;
+using NetArchTest.Rules;
+
+namespace EMS.Modules.Attendance.ArchitectureTests.Presentation;
+public class PresentationTests
+{
+    [Fact]
+    public void IntegrationEventConsumer_Should_BeSealed()
+    {
+        Types.InAssembly(BaseTest.PresentationAssembly)
+            .That()
+            .ImplementInterface(typeof(IConsumer<>))
+            .Should()
+            .BeSealed()
+            .GetResult()
+            .ShouldBeSuccessful();
+    }
+
+    [Fact]
+    public void IntegrationEventConsumer_ShouldHave_NameEndingWith_IntegrationEventConsumer()
+    {
+        Types.InAssembly(BaseTest.PresentationAssembly)
+            .That()
+            .ImplementInterface(typeof(IConsumer<>))
+            .Should()
+            .HaveNameEndingWith("IntegrationEventConsumer")
+            .GetResult()
+            .ShouldBeSuccessful();
+    }
+}
