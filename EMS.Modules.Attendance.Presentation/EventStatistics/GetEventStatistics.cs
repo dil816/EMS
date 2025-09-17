b7ad25a101ef1ba90ec2b644b71058a43d1e596c
dil816 <Rivindudilochana18329@gmail.com>
2025-09-17 16:30:53 +0530

    endpoint added for event statics

---
+++ EMS.Modules.Attendance.Presentation/EventStatistics/GetEventStatistics.cs
@@ -0,0 +1,24 @@
+﻿using EMS.Common.Domain;
+using EMS.Common.Presentation.ApiResults;
+using EMS.Common.Presentation.EndPoints;
+using EMS.Modules.Attendance.Application.Events.EventStatistics.GetEventStatistics;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace EMS.Modules.Attendance.Presentation.EventStatistics;
+internal sealed class GetEventStatistics : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("event-statistics/{id}", async (Guid id, ISender sender) =>
+        {
+            Result<EventStatisticsResponse> result = await sender.Send(new GetEventStatisticsQuery(id));
+
+            return result.Match(Results.Ok, ApiResults.Problem);
+        })
+        .RequireAuthorization(Permissions.GetEventStatistics)
+        .WithTags(Tags.EventStatistics);
+    }
+}
