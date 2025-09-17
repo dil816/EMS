ffce00bbd36a5ed1d467b75003360b1c894a9559
dil816 <Rivindudilochana18329@gmail.com>
2025-09-17 23:23:26 +0530

    intergation event and handler added

---
+++ EMS.Modules.Events.Application/Events/CancelEvent/EventCanceledDomainEventHandler.cs
@@ -0,0 +1,21 @@
+﻿using EMS.Common.Application.EventBus;
+using EMS.Common.Application.Messaging;
+using EMS.Modules.Events.Domain.Events;
+using EMS.Modules.Events.IntegrationEvents;
+
+namespace EMS.Modules.Events.Application.Events.CancelEvent;
+internal sealed class EventCanceledDomainEventHandler(IEventBus eventBus)
+    : DomainEventHandler<EventCanceledDomainEvent>
+{
+    public override async Task Handle(
+        EventCanceledDomainEvent domainEvent,
+        CancellationToken cancellationToken = default)
+    {
+        await eventBus.PublishAsync(
+            new EventCanceledIntegrationEvent(
+                domainEvent.Id,
+                domainEvent.OccurredOnUtc,
+                domainEvent.EventId),
+            cancellationToken);
+    }
+}
---
+++ EMS.Modules.Events.IntegrationEvents/EventCanceledIntegrationEvent.cs
@@ -0,0 +1,13 @@
+﻿using EMS.Common.Application.EventBus;
+
+namespace EMS.Modules.Events.IntegrationEvents;
+public sealed class EventCanceledIntegrationEvent : IntegrationEvent
+{
+    public EventCanceledIntegrationEvent(Guid id, DateTime occurredOnUtc, Guid eventId)
+        : base(id, occurredOnUtc)
+    {
+        EventId = eventId;
+    }
+
+    public Guid EventId { get; init; }
+}
