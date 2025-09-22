fd784f254435d66d5bcfafcd6368325b8a515620
dil816 <Rivindudilochana18329@gmail.com>
2025-09-22 19:16:36 +0530

    json file name fix

--- EMS.IntegrationTests/Abstractions/IntegrationTestWebAppFactory.cs
+++ EMS.IntegrationTests/Abstractions/IntegrationTestWebAppFactory.cs
@@ -25,7 +25,7 @@ public class IntegrationTestWebAppFactory : WebApplicationFactory<Program>, IAsy
     private readonly KeycloakContainer _keycloakContainer = new KeycloakBuilder()
         .WithImage("quay.io/keycloak/keycloak:latest")
         .WithResourceMapping(
-            new FileInfo("evently-realm-export.json"),
+            new FileInfo("ems-realm-export.json"),
             new FileInfo("/opt/keycloak/data/import/realm.json"))
         .WithCommand("--import-realm")
         .Build();
@@ -36,7 +36,7 @@ public class IntegrationTestWebAppFactory : WebApplicationFactory<Program>, IAsy
         Environment.SetEnvironmentVariable("ConnectionStrings:Cache", _redisContainer.GetConnectionString());
 
         string keycloakAddress = _keycloakContainer.GetBaseAddress();
-        string keyCloakRealmUrl = $"{keycloakAddress}realms/evently";
+        string keyCloakRealmUrl = $"{keycloakAddress}realms/ems";
 
         Environment.SetEnvironmentVariable(
             "Authentication:MetadataAddress",
@@ -49,7 +49,7 @@ public class IntegrationTestWebAppFactory : WebApplicationFactory<Program>, IAsy
         {
             services.Configure<KeyCloakOptions>(o =>
             {
-                o.AdminUrl = $"{keycloakAddress}admin/realms/evently/";
+                o.AdminUrl = $"{keycloakAddress}admin/realms/ems/";
                 o.TokenUrl = $"{keyCloakRealmUrl}/protocol/openid-connect/token";
             });
         });
