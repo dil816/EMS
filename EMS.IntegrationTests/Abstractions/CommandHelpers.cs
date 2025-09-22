fd867056073c16034fb04b138e81a3e4458f3bca
dil816 <Rivindudilochana18329@gmail.com>
2025-09-22 19:05:44 +0530

    new Integration test adde for system

---
+++ EMS.IntegrationTests/Abstractions/CommandHelpers.cs
@@ -0,0 +1,39 @@
+using Bogus;
+using EMS.Common.Domain;
+using EMS.Modules.Ticketing.Application.Events.CreateEvent;
+using FluentAssertions;
+using MediatR;
+
+namespace EMS.IntegrationTests.Abstractions;
+
+internal static class CommandHelpers
+{
+    internal static async Task CreateEventAsync(
+        this ISender sender,
+        Guid eventId,
+        Guid ticketTypeId,
+        decimal quantity)
+    {
+        var faker = new Faker();
+
+        var ticketType = new CreateEventCommand.TicketTypeRequest(
+            ticketTypeId,
+            eventId,
+            faker.Music.Genre(),
+            faker.Random.Decimal(),
+            "USD",
+            quantity);
+
+        Result result = await sender.Send(new CreateEventCommand(
+            eventId,
+            faker.Music.Genre(),
+            faker.Music.Genre(),
+            faker.Address.FullAddress(),
+            DateTime.UtcNow,
+            null,
+            [ticketType]));
+
+        result.IsSuccess.Should().BeTrue();
+    }
+}
+
---
+++ EMS.IntegrationTests/Abstractions/IntegrationTestCollection.cs
@@ -0,0 +1,5 @@
+namespace EMS.IntegrationTests.Abstractions;
+
+[CollectionDefinition(nameof(IntegrationTestCollection))]
+public sealed class IntegrationTestCollection : ICollectionFixture<IntegrationTestWebAppFactory>;
+
