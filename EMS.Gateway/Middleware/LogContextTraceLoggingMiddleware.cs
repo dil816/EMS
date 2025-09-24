4082062b483fec56c83a4c8cb1217549d6fdbae0
dil816 <Rivindudilochana18329@gmail.com>
2025-09-24 11:00:45 +0530

    Reverse proxy configured

---
+++ EMS.Gateway/Middleware/LogContextTraceLoggingMiddleware.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using Serilog.Context;
+
+
+namespace EMS.Gateway.Middleware;
+
+internal sealed class LogContextTraceLoggingMiddleware(RequestDelegate next)
+{
+    public Task Invoke(HttpContext context)
+    {
+        string traceId = Activity.Current?.TraceId.ToString();
+
+        using (LogContext.PushProperty("TraceId", traceId))
+        {
+            return next.Invoke(context);
+        }
+    }
+}
