c67c79df37e9aaad86748448fc306340056c3642
dil816 <Rivindudilochana18329@gmail.com>
2025-09-24 14:28:53 +0530

    ticket.api related tests removed

--- EMS.IntegrationTests/RegisterUser/RegisterUserTests.cs
+++ EMS.IntegrationTests/RegisterUser/RegisterUserTests.cs
@@ -2,7 +2,6 @@
 using EMS.Common.Domain;
 using EMS.IntegrationTests.Abstractions;
 using EMS.Modules.Attendance.Application.Attendees.GetAttendee;
-using EMS.Modules.Ticketing.Application.Customers.GetCustomer;
 using EMS.Modules.Users.Application.Users.RegisterUser;
 using FluentAssertions;
 
@@ -12,37 +11,6 @@ public sealed class RegisterUserTests : BaseIntegrationTest
     public RegisterUserTests(IntegrationTestWebAppFactory factory)
         : base(factory)
     {
-    }
-
-    [Fact]
-    public async Task RegisterUser_Should_PropagateToTicketingModule()
-    {
-        // Register user
-        var command = new RegisterUserCommand(
-            Faker.Internet.Email(),
-            Faker.Internet.Password(6),
-            Faker.Name.FirstName(),
-            Faker.Name.LastName());
-
-        Result<Guid> userResult = await Sender.Send(command);
-
-        userResult.IsSuccess.Should().BeTrue();
-
-        // Get customer
-        Result<CustomerResponse> customerResult = await Poller.WaitAsync(
-            TimeSpan.FromSeconds(15),
-            async () =>
-            {
-                var query = new GetCustomerQuery(userResult.Value);
-
-                Result<CustomerResponse> customerResult = await Sender.Send(query);
-
-                return customerResult;
-            });
-
-        // Assert
-        customerResult.IsSuccess.Should().BeTrue();
-        customerResult.Value.Should().NotBeNull();
     }
 
     [Fact]
